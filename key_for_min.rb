37822b634fb5dadc0cc349a59c1ce64680cc7081
casual-karma-e108 <akravecas@yahoo.com>
2021-02-24 17:52:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,12 +6,15 @@ require 'pry'
 
 def key_for_min_value(name_hash)
   min = nil
+  key_for_min = nil
   name_hash.each do |x,y|
     if min == nil
       min = y
       if y < min
         min = y
+        key_for_min = x
       end
     end
+    key_for_min
   end
 end
