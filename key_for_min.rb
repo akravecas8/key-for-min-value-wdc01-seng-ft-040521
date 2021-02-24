cd3dd9d38e763f4e54ea327d6b0af8a50b5506f0
casual-karma-e108 <akravecas@yahoo.com>
2021-02-24 18:14:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
   min = nil
   key_for_min = nil
   name_hash.each do |x,y|
-    binding.pry
+   # binding.pry
     if min == nil
       min = y
     #  key_for_min= x
